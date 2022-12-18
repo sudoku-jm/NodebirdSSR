fe4176afffd30c82e1a46ae8e5c226cd66a0e5fa
sudoku-jm <sudoku8386@gmail.com>
2022-12-18 19:03:59 +0900

    front producton ip

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,1 +1,1 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://localhost:5500' : 'http://localhost:5500';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://43.201.10.62' : 'http://localhost:5500';
